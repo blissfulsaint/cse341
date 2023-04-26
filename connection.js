108acec91535733f866a385d8f47c22fee251846
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-04-26 15:56:12 -0600

    learning material work

---
+++ connection.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function main() {
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jtoy9uh.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+    
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+main().catch(console.error);
