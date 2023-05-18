36cd5603993a0610bbbb833a78a1f2afc28b7457
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-05-17 20:32:54 -0600

    swagger stuff

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const routes = require('express').Router();
 const namesController = require('../controllers');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 
 routes.get('/', namesController.bestFriend);
 
@@ -9,3 +12,6 @@ routes.get('/charlotte', namesController.bestGirlFriend);
 routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
