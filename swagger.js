e6c53653d7f6d54046483ea00201f1bcfab53233
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-05-20 18:41:47 -0600

    quick change

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Temple API',
     },
-    host: 'localhost:8080',
+    host: 'https://cse341-g8lt.onrender.com/',
     schemes: ['h55p'],
 };
 
